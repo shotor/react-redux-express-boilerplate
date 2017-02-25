a31c6d7c15d44505033575828222583e2f9fa7e2
Shotor Morgh <shotor@protonmail.com>
2017-02-25 19:14:01 +0100

    rename Root to root

--- app/index.js
+++ app/index.js
@@ -92,10 +92,10 @@ if (module.hot) {
      }
    };
 
-  module.hot.accept('./Root', () => {
+  module.hot.accept('./root', () => {
     // If you use Webpack 2 in ES modules mode, you can
     // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./Root').default;
+    const NextApp = require('./root').default;
 
     render(
       <AppContainer errorReporter={Redbox}>

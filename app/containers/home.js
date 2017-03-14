b2a07bd67974845cec967b277f5a8ad4fac85a36
Kian Khoshnoodpour <kian.khoshnoodpour@marlink.com>
2017-03-14 22:59:48 +0100

    integrate react editor

--- app/containers/home.js
+++ app/containers/home.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
+import RichTextEditor from 'react-rte'
+
 import * as EntityActions from 'actions/entity-actions'
 
 class Home extends Component {
@@ -11,31 +13,36 @@ class Home extends Component {
         entities: PropTypes.object.isRequired
     }
 
+    constructor() {
+        super()
+        this.state = {
+            editor_value: RichTextEditor.createEmptyValue()
+        }
+    }
+
     componentDidMount() {
         const { refresh_entities } = this.props
         // use this to load page data
         //refresh_entities('documents')
     }
 
+    onChangeEditor(editor_value) {
+        this.setState({editor_value});
+    }
+
     render() {
 
         // access entities
         // also possible to simplify in mapStateToPropss
-        const { entities: { documents }} = this. props
+        const { entities: { documents } } = this.props
 
         return (
-            <div>
+            <div id="container-home">
 
-                <div className="jumbotron">
-                    <div className="container">
-                        <h1>Hello, world!</h1>
-                        <p>
-                            <a className="btn btn-primary btn-lg" href="#" role="button">
-                                Learn more
-                        </a>
-                        </p>
-                    </div>
-                </div>
+                <RichTextEditor
+                    value={this.state.editor_value}
+                    onChange={(new_value) => this.onChangeEditor(new_value)}
+                />
 
             </div>
         )
